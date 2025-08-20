1bbe8cd749c8507bc7e70ce2e1746b23f6e16625
Edward Stanley <edward.j.stanley@icloud.com>
2025-08-20 12:35:24 +1000

    Initial Commit

---
+++ Mercury/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Mercury
+//
+//  Created by Edward Stanley on 20/8/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ MercuryUITests/MercuryUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MercuryUITestsLaunchTests.swift
+//  MercuryUITests
+//
+//  Created by Edward Stanley on 20/8/2025.
+//
+
+import XCTest
+
+final class MercuryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
